b1d142f9124288b7fed3da0d3e4b293b49f981b5
Axel Sorenson <AxelPSorenson@gmail.com>
2023-12-22 03:13:42 -0800

    Minor update to keymap

--- keyboards/beekeeb/piantor/keymaps/axelcool1234/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/axelcool1234/keymap.c
@@ -5,7 +5,7 @@
 
 /* Layers */
 enum layers {
-    SIMIMAK,
+    SEMIMAK,
     QWERTY,
     SYMBOLS,
     FUN
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┤   │   │   ├───┘
       *                       └───┘   └───┘
       */
-    [SIMIMAK] = LAYOUT_split_3x6_3(
+    [SEMIMAK] = LAYOUT_split_3x6_3(
         KC_ESC,  KC_F,    KC_L,    KC_H,    KC_V,    KC_Z,                               KC_Q,    KC_W,    KC_U,    KC_O,    KC_Y,    KC_TRNS,
         KC_LCTL, KC_S,    KC_R,    KC_N,    KC_T,    KC_K,                               KC_C,    KC_D,    KC_E,    KC_A,    KC_I,    KC_SCLN,
         KC_LSFT, KC_X,    KC_QUOT, KC_B,    KC_M,    KC_J,                               KC_P,    KC_G,    KC_COMM, KC_DOT,  KC_SLSH, DF(QWERTY),
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, DF(SIMIMAK),
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, DF(SEMIMAK),
                                             KC_LGUI, KC_SPC, KC_BSPC,          KC_ENT, KC_SPC, KC_RALT
     ),
 
