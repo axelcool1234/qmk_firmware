e8238996751fcce3ea90c7b81cb874845b2224c9
Axel Sorenson <AxelPSorenson@gmail.com>
2023-12-22 02:26:54 -0800

    Simimak setup along with Via support

--- keyboards/beekeeb/piantor/keymaps/axelcool1234/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/axelcool1234/keymap.c
@@ -30,16 +30,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [SIMIMAK] = LAYOUT_split_3x6_3(
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
+        KC_ESC,  KC_F,    KC_L,    KC_H,    KC_V,    KC_Z,                               KC_Q,    KC_W,    KC_U,    KC_O,    KC_Y,    KC_TRNS,
+        KC_LCTL, KC_S,    KC_R,    KC_N,    KC_T,    KC_K,                               KC_C,    KC_D,    KC_E,    KC_A,    KC_I,    KC_SCLN,
+        KC_LSFT, KC_X,    KC_QUOT, KC_B,    KC_M,    KC_J,                               KC_P,    KC_G,    KC_COMM, KC_DOT,  KC_SLSH, DF(QWERTY),
+                                            KC_LSFT, KC_SPC, KC_DEL,          KC_DEL, KC_SPC, KC_RSFT
     ),
 
     [QWERTY] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, DF(SIMIMAK),
                                             KC_LGUI, KC_SPC, KC_BSPC,          KC_ENT, KC_SPC, KC_RALT
     ),
 
