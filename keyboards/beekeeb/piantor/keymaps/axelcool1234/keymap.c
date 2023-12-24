7168f738176b86f68f4cacc92f6fd94d663e2618
Axel Sorenson <AxelPSorenson@gmail.com>
2023-12-24 06:44:00 -0800

    Added Miryoku features. May not end up using homerow mods due to the noticeable delay when typing.

--- keyboards/beekeeb/piantor/keymaps/axelcool1234/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/axelcool1234/keymap.c
@@ -5,30 +5,68 @@
 
 /* Layers */
 enum layers {
+    // base
     _SEMIMAK,
     _QWERTY,
     _GAME,
-    _SYM,
+    // stack
     _NAV,
-    _FUN
+    _MOUSE,
+    _MEDIA,
+    _NUM,
+    _SYM,
+    _FUN,
+    _BUTTON
 };
 
-/* Keycodes */
+/* Misc */
+#define XXXXXXXX XXXXXXX
 
-/* Combos */
+/* Homerow mods */
+#define HOME_CAP QK_CAPS_WORD_TOGGLE
+
+// Left homerow mods
+#define HOME_S LGUI_T(KC_S)
+#define HOME_R LALT_T(KC_R)
+#define HOME_N LSFT_T(KC_N)
+#define HOME_T LCTL_T(KC_T)
+
+// Right homerow mods
+#define HOME_D RCTL_T(KC_D)
+#define HOME_E RSFT_T(KC_E)
+#define HOME_A LALT_T(KC_A)
+#define HOME_I RGUI_T(KC_I)
+
+/* Thumb cluster mods */
+#define P_BUTTON OSL(_BUTTON)
+
+// Left thumb cluster mods
+#define TH_SPC LT(_NAV,   KC_SPC)
+#define TH_TAB LT(_MOUSE, KC_TAB)
+#define TH_ESC LT(_MEDIA, KC_ESC)
+
+// Right thumb cluster mods
+#define TH_BSPC LT(_NUM, KC_BSPC)
+#define TH_ENT  LT(_SYM, KC_ENT)
+#define TH_DEL  LT(_FUN, KC_DEL)
+
+/* Switch base */
+#define D_QWERTY  DF(_QWERTY)
+#define D_GAME    DF(_GAME)
+#define D_SEMIMAK DF(_SEMIMAK)
 
 /* Keymaps */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base Layers */
     [_SEMIMAK] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_ESC,   KC_F,   KC_L,     KC_H,    KC_V,    KC_Z,      /*-------*/        KC_Q,    KC_W,    KC_U,    KC_O,   KC_Y,    KC_ESC,
+       KC_ESC,   KC_F,   KC_L,     KC_H,    KC_V,    KC_Z,       /*-------*/       KC_Q,    KC_W,    KC_U,    KC_O,   KC_Y,    KC_ESC,
     //|--------+--------+--------+--------+--------+--------|     |SEMIMAK|      |--------+--------+--------+--------+--------+--------|
-        KC_LCTL,  KC_S,   KC_R,     KC_N,    KC_T,    KC_K,      /*-------*/        KC_C,    KC_D,    KC_E,    KC_A,   KC_I,    KC_SCLN,
+       HOME_CAP, KC_S,   KC_R,     KC_N,    KC_T,    KC_K,       /*-------*/       KC_C,    KC_D,    KC_E,    KC_A,   KC_I,    KC_SCLN,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LSFT,  KC_X,   KC_QUOT,  KC_B,    KC_M,    KC_J,                         KC_P,    KC_G,    KC_COMM, KC_DOT, KC_SLSH, DF(_QWERTY),
+       P_BUTTON, KC_X,   KC_QUOT,  KC_B,    KC_M,    KC_J,                         KC_P,    KC_G,    KC_COMM, KC_DOT, KC_SLSH, D_QWERTY,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           MO(_NAV),MO(_SYM), KC_ENT,    KC_BSPC, KC_SPC, KC_RSFT
+                                            KC_ESC,  KC_SPC, KC_TAB,      KC_ENT, KC_BSPC, KC_DEL
                                         //`--------------------------'  `--------------------------'
     ),
 
@@ -38,60 +76,102 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------+--------+--------+--------+--------+--------|      |QWERTY|      |--------+--------+--------+--------+--------+--------|
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        /*------*/       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, DF(_GAME),
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, D_GAME,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_LGUI, KC_SPC, KC_BSPC,    KC_ENT, KC_SPC, KC_RALT
+                                            KC_LGUI, KC_SPC, KC_BSPC,     KC_ENT,  KC_SPC,  KC_RALT
                                         //`--------------------------'  `--------------------------'
     ),
 
     [_GAME] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_NO,   KC_NO,    KC_Q,    KC_W,    KC_E,    KC_R,       /*------*/        KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,
+       XXXXXXXX,  KC_G,   KC_Q,    KC_W,    KC_E,    KC_R,        /*------*/      XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
     //|--------+--------+--------+--------+--------+--------|      |Gaming|      |--------+--------+--------+--------+--------+--------|
-        KC_NO,   KC_G,     KC_S,    KC_A,    KC_D,    KC_F,       /*------*/        KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,  DF(_QWERTY),
+       XXXXXXXX, KC_LSFT,   KC_S,    KC_A,    KC_D,    KC_F,        /*------*/      XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,D_QWERTY,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,   KC_NO,    KC_Z,    KC_X,    KC_C,    KC_NO,                        KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,  DF(_SEMIMAK),
+       XXXXXXXX,XXXXXXXX, KC_Z,    KC_X,    KC_C,   XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,D_SEMIMAK,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO,  MO(_SYM),  KC_NO,    KC_DEL, KC_SPC, KC_RSFT
+                                           XXXXXXXX, KC_SPC, XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX
                                         //`--------------------------'  `--------------------------'
     ),
 
     /* Stack Layers */
-    [_SYM] = LAYOUT_split_3x6_3(
-    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_NO,   KC_9,    KC_5,     KC_1,    KC_3,   KC_7,        /*-------*/      KC_6,     KC_2,   KC_0,     KC_4,   KC_8,     KC_NO,
-    //|--------+--------+--------+--------+--------+--------|      |Symbols|     |--------+--------+--------+--------+--------+--------|
-        KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,       /*-------*/      KC_NO,    KC_NO,  KC_NO,    KC_NO,  KC_NO,    KC_NO,
-    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,                        KC_NO,    KC_NO,  KC_NO,    KC_NO,  KC_NO,    KC_NO,
-    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
-                                        //`--------------------------'  `--------------------------'
-    ),
-
     [_NAV] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     /*----------*/     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    //|--------+--------+--------+--------+--------+--------|    |Navigation|    |--------+--------+--------+--------+--------+--------|
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     /*----------*/     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_TRNS, KC_TRNS, KC_TRNS,   MO(_FUN), KC_TRNS, KC_TRNS
+                                           XXXXXXXX,XXXXXXXX,XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX
                                         //`--------------------------'  `--------------------------'
     ),
-
+    [_MOUSE] = LAYOUT_split_3x6_3(
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                           XXXXXXXX,XXXXXXXX,XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX
+                                        //`--------------------------'  `--------------------------'
+    ),
+    [_MEDIA] = LAYOUT_split_3x6_3(
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                           XXXXXXXX,XXXXXXXX,XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX
+                                        //`--------------------------'  `--------------------------'
+    ),
+    [_NUM] = LAYOUT_split_3x6_3(
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                           XXXXXXXX,XXXXXXXX,XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX
+                                        //`--------------------------'  `--------------------------'
+    ),
+    [_SYM] = LAYOUT_split_3x6_3(
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                           XXXXXXXX,XXXXXXXX,XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX
+                                        //`--------------------------'  `--------------------------'
+    ),
     [_FUN] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,   KC_NO,      /*---------*/     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  QK_CLEAR_EEPROM,
-    //|--------+--------+--------+--------+--------+--------|     |Functions|    |--------+--------+--------+--------+--------+--------|
-        KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,   KC_NO,      /*---------*/     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  QK_MAKE,
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  QK_BOOT,
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+                                           XXXXXXXX,XXXXXXXX,XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX
                                         //`--------------------------'  `--------------------------'
-    )
+    ),
+    [_BUTTON] = LAYOUT_split_3x6_3(
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                           XXXXXXXX,XXXXXXXX,XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX
+                                        //`--------------------------'  `--------------------------'
+    ),
 };
 
 /* Additional Functionality */
