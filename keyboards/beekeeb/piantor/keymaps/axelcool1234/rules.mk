1003b5d12c151265ea4893bab31b8016845337ff
Axel Sorenson <AxelPSorenson@gmail.com>
2024-01-02 18:42:41 -0800

    Removed key overrides in favor for custom shift keys. Added sentence case and edited source code so it'll work for my custom shifted keys.

--- keyboards/beekeeb/piantor/keymaps/axelcool1234/rules.mk
+++ keyboards/beekeeb/piantor/keymaps/axelcool1234/rules.mk
@@ -1,9 +1,10 @@
 VIA_ENABLE  = yes
 CAPS_WORD_ENABLE = yes
 TRI_LAYER_ENABLE = yes
-KEY_OVERRIDE_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
 LEADER_ENABLE = yes
 
 SRC += oneshot.c
+SRC += custom_shift_keys.c
+SRC += sentence_case.c
