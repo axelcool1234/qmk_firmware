5fe2b0cb60e9092de1ed3cf809335c6aa1011129
Xuming Zeng <xsznix@gmail.com>
2025-08-27 20:53:20 -0500

    Remove RSTHD and QWERTY fully. g_f -> g_s

--- keyboards/ergodox_ez/keymaps/xsznix/keymap.c
+++ keyboards/ergodox_ez/keymaps/xsznix/keymap.c
@@ -3,71 +3,23 @@
 #include "action_layer.h"
 #include "version.h"
 
-#define RSTHD_OSX   0
-#define PARACETAMAK 1
-#define QWERTY      2
-#define NUMPAD      3
-#define MEDIA_OSX   4
-#define SPECIAL     5
-#define P9K_SYM     6
-#define MACRO_OSX   7
+#define AFTERBURNER 0
+#define NUMPAD      1
+#define MEDIA_OSX   2
+#define P9K_SYM     3
+#define MACRO_OSX   4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-/* RSTHD for OS X
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   7  |   8  |   9  |   0  |   5  | LCtl |           | RCtl |   6  |   1  |   2  |   3  |   4  |   \    |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   J  |   C  |   Y  |   F  |   K  | LAlt |           | RAlt |   Z  |   L  |   ,  |   U  |   Q  |   =    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Esc    |   R  |   S  |   T  |   H  |   D  |------|           |------|   M  |   N  |   A  |   I  |   O  |   '    |
- * |--------+------+------+------+------+------| LGui |           | RGui |------+------+------+------+------+--------|
- * | LShift |   /  |   V  |   G  |   P  |   B  |      |           |      |   X  |   W  |   .  |   ;  |   -  | RShift |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Macro|  M1  |  M2  | Down |  Up  |                                       | Left | Right| MSpc |QWERTY| ^Spc |
- *   `----------------------------------'                                       `----------------------------------'
- *                                       ,--------------.       ,--------------.
- *                                       | Numpad| Media|       | Media| Numpad|
- *                                ,------|-------|------|       |------+-------+-------.
- *                                |      |       | P9K  |       |QWERTY|       |       |
- *                                |  E   | Bksp  |------|       |------| Enter | Space |
- *                                |      |       | Speci|       | Speci|       |       |
- *                                `---------------------'       `----------------------'
- */
-// If it accepts an argument (i.e, is a function), it doesn't need KC_.
-// Otherwise, it needs KC_*
-[RSTHD_OSX] = LAYOUT_ergodox(
-        // left hand
-        KC_GRV,  KC_7,    KC_8, KC_9, KC_0, KC_5, KC_LCTL,
-        KC_TAB,  KC_J,    KC_C, KC_Y, KC_F, KC_K, KC_LALT,
-        KC_ESC,  KC_R,    KC_S,  KC_T, KC_H, KC_D,
-        KC_LSFT, KC_SLSH, KC_V,  KC_G, KC_P, KC_B, KC_LGUI,
-        MO(MACRO_OSX), KC_BTN1, KC_BTN2, KC_DOWN, KC_UP,
-                                        MO(NUMPAD), MO(MEDIA_OSX),
-                                                    TG(PARACETAMAK),
-                                    KC_E,  KC_BSPC, MO(SPECIAL),
-        // right hand
-        KC_RCTL, KC_6, KC_1, KC_2,    KC_3,      KC_4,    KC_BSLS,
-        KC_RALT, KC_Z, KC_L, KC_COMM, KC_U,      KC_Q,    KC_EQUAL,
-                 KC_M, KC_N, KC_A,    KC_I,      KC_O,    KC_QUOTE,
-        KC_RGUI, KC_X, KC_W, KC_DOT,  KC_SCLN, KC_MINS, KC_RSFT,
-                       KC_LEFT, KC_RIGHT, LALT(KC_SPC), TG(QWERTY), LCTL(KC_SPC),
-        MO(MEDIA_OSX),   MO(NUMPAD),
-        TG(QWERTY),
-        MO(SPECIAL), KC_ENT, KC_SPC
-    ),
-
-/* Paracetamak
+/* Afterburner
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Esc    |  7   |  8   |  9   |  0   |  5   |M-Bksp|           |M-Del |  6   |  1   |  2   |  3   |  4   |  \     |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |  J   |  B   |  G   |  V   |  K   | Alt  |           | Alt  |  '   |  W   |  O   |  U   |  ,   | Bksp   |
+ * | Tab    |  J   |  B   |  G   |  V   |  X   | Alt  |           | Alt  |  '   |  W   |  O   |  U   |  ,   | Bksp   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Q      |  H   |  N   |  S   |  T   |  M   |------|           |------|Magic |SkpMgc|  A   |  E   |  I   |  -     |
  * |--------+------+------+------+------+------| Cmd  |           | Cmd  |------+------+------+------+------+--------|
- * | LShift |  Y   |  P   |  F   |  D   |  X   |      |           |      |  Z   |  C   |  /   |  ;   |  .   | RShift |
+ * | LShift |  Y   |  P   |  F   |  D   |  K   |      |           |      |  Z   |  C   |  /   |  ;   |  .   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | Macro|  M1  |  M2  | Down |  Up  |                                       | Left | Right|  [   |  ]   |  =   |
  *   `----------------------------------'                                       `----------------------------------'
@@ -79,15 +31,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      | Sym  |       | Sym  |      |      |
  *                                 `--------------------'       `--------------------'
  */
-[PARACETAMAK] = LAYOUT_ergodox(
+[AFTERBURNER] = LAYOUT_ergodox(
        // left hand
        KC_ESC,        KC_7,    KC_8,    KC_9,    KC_0,    KC_5,    LALT(KC_BSPC),
-       KC_TAB,        KC_J,    KC_B,    KC_G,    KC_V,    KC_K,    KC_LALT,
+       KC_TAB,        KC_J,    KC_B,    KC_G,    KC_V,    KC_X,    KC_LALT,
        KC_Q,          KC_H,    KC_N,    KC_S,    KC_T,    KC_M,
-       KC_LSFT,       KC_Y,    KC_P,    KC_F,    KC_D,    KC_X,    KC_LGUI,
+       KC_LSFT,       KC_Y,    KC_P,    KC_F,    KC_D,    KC_K,    KC_LGUI,
        MO(MACRO_OSX), KC_BTN1, KC_BTN2, KC_DOWN, KC_UP,
-                                        KC_LCTL, MO(NUMPAD),
-                                                 TG(PARACETAMAK),
+                                        KC_LCTL, MO(MEDIA_OSX),
+                                                 MO(NUMPAD),
                                   KC_R, KC_L,    MO(P9K_SYM),
        // right hand
   LALT(KC_DEL), KC_6,    KC_1,    KC_2,    KC_3,    KC_4,    KC_BSLS,
@@ -96,50 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_RGUI, KC_Z,    KC_C,    KC_SLSH, KC_SCLN, KC_DOT,  KC_RSFT,
                          KC_LEFT, KC_RIGHT,KC_LBRC, KC_RBRC, KC_EQL,
        MO(MEDIA_OSX), KC_RCTL,
-       TG(QWERTY),
+       MO(NUMPAD),
        MO(P9K_SYM), KC_ENT, KC_SPC
-    ),
-
-/* QWERTY
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |    =   |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |    -   |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |    '   |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |   \  |      |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 | SPC  |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-[QWERTY] = LAYOUT_ergodox(
-       // left hand
-       KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_TRNS,
-       KC_TRNS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_TRNS,
-       KC_TRNS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-       KC_TRNS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                        KC_TRNS, KC_TRNS,
-                                                 KC_TRNS,
-                                KC_SPC, KC_TRNS, KC_TRNS,
-       // right hand
-       KC_TRNS, KC_6, KC_7,    KC_8,   KC_9,    KC_0,     KC_EQUAL,
-       KC_TRNS, KC_Y, KC_U,    KC_I,   KC_O,    KC_P,     KC_MINUS,
-                KC_H, KC_J,    KC_K,   KC_L,    KC_SCLN,  KC_QUOT,
-       KC_TRNS, KC_N, KC_M,    KC_COMM,KC_DOT,  KC_SLSH,  KC_TRNS,
-                      KC_TRNS, KC_TRNS, KC_BSLS,KC_TRNS,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
 /* Function and Numpad
@@ -221,48 +131,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TRNS, LCTL(LALT(KC_6)),       LCTL(LALT(KC_7)),       LCTL(LALT(KC_8)),       LCTL(LALT(KC_9)),       LCTL(LALT(KC_0)),
        KC_TRNS, KC_TRNS, LCTL(LALT(KC_1)),       LCTL(LALT(KC_2)),       LCTL(LALT(KC_3)),       LCTL(LALT(KC_4)),       LCTL(LALT(KC_5)),
                          KC_MPRV,                KC_MPLY,                KC_MNXT,                KC_TRNS,                KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-
-/* Special
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   <  |   >  |   [  |   ]  |   \  |      |           |      |M-del |   :  |      |   {  |   }  |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   !  |   @  |   #  |   $  |   %  |------|           |------|   ^  |   &  |   *  |   (  |   )  |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |   ~  |   _  |   "  |   |  |      |           |      |   ?  |   +  |      |      |      |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-[SPECIAL] = LAYOUT_ergodox(
-       // left hand
-       KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,
-       KC_TRNS, S(KC_COMM), S(KC_DOT), KC_LBRC, KC_RBRC,    KC_BSLS, KC_TRNS,
-       KC_TRNS, KC_EXLM,    KC_AT,     KC_HASH,  KC_DLR,    KC_PERC,
-       KC_TRNS, KC_TRNS,    KC_TILD,   KC_UNDS, S(KC_QUOT), KC_PIPE, KC_TRNS,
-       KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS,
-                                        KC_TRNS, KC_TRNS,
-                                                 KC_TRNS,
-                               KC_DEL,  KC_TRNS, KC_TRNS,
-       // right hand
-       KC_TRNS, KC_TRNS,       KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, LALT(KC_BSPC), KC_COLN,     KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS,
-                KC_CIRC,       KC_AMPR,     KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
-       KC_TRNS, S(KC_SLSH),    S(KC_EQUAL), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                               KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
@@ -430,10 +298,10 @@ uint16_t get_skip_alt_repeat_key_keycode_user(uint16_t keycode, uint8_t mods) {
     case KC_B: return KC_N;
     case KC_D: return KC_T;
     case KC_F: return KC_S;
-    case KC_G: return KC_F;
+    case KC_G: return KC_S;
     case KC_H: return KC_Y;
     case KC_J: return KC_Y;
-    case KC_K: return KC_D;
+    case KC_K: return KC_T;
     case KC_L: return KC_R;
     case KC_M: return KC_K;
     case KC_O: return KC_A;
@@ -464,28 +332,4 @@ void matrix_init_user(void) {
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
-    uint8_t layer = biton32(layer_state);
-
-    ergodox_board_led_off();
-    ergodox_right_led_1_off();
-    ergodox_right_led_2_off();
-    ergodox_right_led_3_off();
-
-    if (layer_state & (1 << PARACETAMAK)) {
-      ergodox_right_led_1_on();
-    }
-
-    if (layer_state & (1 << QWERTY)) {
-      ergodox_right_led_2_on();
-    }
-
-    switch (layer) {
-      case RSTHD_OSX:
-      case PARACETAMAK:
-      case QWERTY:
-      break;
-
-      default:
-      ergodox_right_led_3_on();
-    }
 };
