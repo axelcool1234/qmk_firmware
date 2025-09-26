dc61cf8741370133899cf9f0449d71630aa28396
Axel Sorenson <AxelPSorenson@gmail.com>
2025-09-26 03:27:35 -0600

    Implemented base layer with nothing special yet

--- keyboards/beekeeb/piantor/keymaps/axel_lab/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/axel_lab/keymap.c
@@ -5,14 +5,18 @@
 
 #include QMK_KEYBOARD_H
 
-/* Layers */
+// TODO:
+// - Oxidization: https://nullp.tr/posts/oxidising-my-keyboard/#the-fruits-of-our-labour
+
+///--- Layers ---///
+/// The following defines the layers of the keyboard in order. Used in `keymaps`.
+/// BUNYA:       Base layer. Uses Bunya alt keyboard layout. https://layouts.wiki/layouts/2023/bunya/
 enum layers {
-    /* https://layouts.wiki/layouts/2023/bunya/ */
     _BUNYA,
-    _BUNYA_SHIFT,
 };
 
-/* Misc */
+///--- Alias Macros ---////
+/// i.e., alternative names for other defined macros.
 #define XXXXXXXX  XXXXXXX
 #define ________  _______
 
@@ -20,24 +24,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base Layers */
     [_BUNYA] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,    /*-------*/      XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+       XXXXXXXX,  KC_B,    KC_L,    KC_M,    KC_C,    KC_Z,      /*-------*/        KC_X,    KC_F,    KC_O,    KC_U,  KC_COMMA,XXXXXXXX,
     //|--------+--------+--------+--------+--------+--------|     | BUNYA |      |--------+--------+--------+--------+--------+--------|
-       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,    /*-------*/      XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+       XXXXXXXX,  KC_N,    KC_R,    KC_T,    KC_D,    KC_P,      /*-------*/        KC_Y,    KC_H,    KC_A,    KC_E,    KC_I,  KC_MINUS,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+       XXXXXXXX, KC_SCLN,  KC_J,    KC_Q,    KC_G,    KC_W,                         KC_K,    KC_V,  KC_QUOTE,KC_SLASH, KC_DOT, XXXXXXXX,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           XXXXXXXX,XXXXXXXX,XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX,
-                                        //`--------------------------'  `--------------------------'
-    ),
-    [_BUNYA_SHIFT] = LAYOUT_split_3x6_3(
-    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,    /*-------*/      XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
-    //|--------+--------+--------+--------+--------+--------|     | SHIFT |      |--------+--------+--------+--------+--------+--------|
-       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,    /*-------*/      XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
-    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                     XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
-    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           XXXXXXXX,XXXXXXXX,XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX,
+                                           XXXXXXXX,  KC_S,  XXXXXXXX,   XXXXXXXX,XXXXXXXX,XXXXXXXX
                                         //`--------------------------'  `--------------------------'
     ),
 };
