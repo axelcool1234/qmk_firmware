490691f63fcd8233293c9440b6a3c58d3deae283
Axel Sorenson <AxelPSorenson@gmail.com>
2025-09-26 02:41:36 -0600

    Base for lab keyboard setup

---
+++ keyboards/beekeeb/piantor/keymaps/axel_lab/config.h
@@ -0,0 +1,15 @@
+/* WeAct board */
+#define USB_VBUS_PIN GP29
+
+/* key matrix size */
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 6
+
+/* comfort */
+#define MASTER_RIGHT
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 15
+
+/* misc */
+#define ENABLE_COMPILE_KEYCODE // Allows key that compiles qmk for board
